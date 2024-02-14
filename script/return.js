aba1ec146ec497d2ae9b5049fdb42208eae5f1f8
rimonali123 <rimonali450@gmail.com>
2024-02-14 20:48:22 +0600

    added score life section by function

--- script/return.js
+++ script/return.js
@@ -23,25 +23,49 @@ function handleByKeyboardPress(){
     // console.log(playerPressed, expendAlphabet)
 
     if(playerPressed  === expectAlphabet){
-        console.log('you got a pont')
-        // step -1 get the current score
-        const currentScoreElement = document.getElementById('current score');
-        const currentScoreText = currentScoreElement.innerText;
-        const currentScore = parseInt(currentScoreText)
-        console.log(currentScore);
+        const currentScore = getTextElementValueById('current-score');
+        const updateScore = currentScore + 1;
+        setTextElementValueById('current-score', updateScore);
 
-        // step-2 increase / deployment score
-        const newScore = currentScore + 1;
+         // step new round
+         removeBackgroundColor(expectAlphabet)
+         continueGame();
 
-        // step-3 update the new score
-        currentScoreElement.innerText = newScore;
 
-        // step new round
-        removeBackgroundColor(expectAlphabet)
-        continueGame();
+        // --------------------------------------------------
+        // console.log('you got a pont')
+        // // step -1 get the current score
+        // const currentScoreElement = document.getElementById('current score');
+        // const currentScoreText = currentScoreElement.innerText;
+        // const currentScore = parseInt(currentScoreText)
+        // console.log(currentScore);
+
+        // // step-2 increase / deployment score
+        // const newScore = currentScore + 1;
+
+        // // step-3 update the new score
+        // currentScoreElement.innerText = newScore;
+
+        //  // step new round
+        //  removeBackgroundColor(expectAlphabet)
+        //  continueGame();
     }
     else{
-        console.log('you lost a life')
+        const currentLife = getTextElementValueById('current-life');
+        const updateLife = currentLife - 1;
+        setTextElementValueById('current-life', updateLife);
+        
+
+
+
+        // ------------------------------------------------
+        // console.log('you lost a life')
+        // const currentLifeElement = document.getElementById('current-life');
+        // const currentLifeText = currentLifeElement.innerText;
+        // const currentLife = parseInt(currentLifeText);
+
+        // const newLife = currentLife - 1;
+        // currentLifeElement.innerText = newLife;
     }
 }
 document.addEventListener('keyup', handleByKeyboardPress)
