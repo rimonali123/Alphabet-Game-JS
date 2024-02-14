f48b24bd218b461b472ea55f08c802616a83cb7d
rimonali123 <rimonali450@gmail.com>
2024-02-14 20:15:18 +0600

    added score section js

--- script/return.js
+++ script/return.js
@@ -12,3 +12,36 @@ currentAlphabetElement.innerText = alphabet;
 setBackgroundColor(alphabet)
 
 }
+
+function handleByKeyboardPress(){
+    const playerPressed = event.key;
+    // console.log('player pressed', playerPressed)
+
+    const currentAlphabetElement = document.getElementById('current-operand');
+    const currentAlphabet = currentAlphabetElement.innerText;
+    const expectAlphabet = currentAlphabet.toLowerCase();
+    // console.log(playerPressed, expendAlphabet)
+
+    if(playerPressed  === expectAlphabet){
+        console.log('you got a pont')
+        // step -1 get the current score
+        const currentScoreElement = document.getElementById('current score');
+        const currentScoreText = currentScoreElement.innerText;
+        const currentScore = parseInt(currentScoreText)
+        console.log(currentScore);
+
+        // step-2 increase / deployment score
+        const newScore = currentScore + 1;
+
+        // step-3 update the new score
+        currentScoreElement.innerText = newScore;
+
+        // step new round
+        removeBackgroundColor(expectAlphabet)
+        continueGame();
+    }
+    else{
+        console.log('you lost a life')
+    }
+}
+document.addEventListener('keyup', handleByKeyboardPress)
--- script/utility.js
+++ script/utility.js
@@ -10,10 +10,10 @@ function showElementById(elementId){
 }
 
 function getRandomAlphabet(){
-    const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
+    const alphabetString = 'abcdefghijklmnopqrstuvwxyz/';
     const alphabets = alphabetString.split('');
 
-    const randomNumber = Math.random()*25;
+    const randomNumber = Math.random()*26;
     const index = Math.round(randomNumber);
 
     const alphabet = alphabets[index];
@@ -24,4 +24,8 @@ function getRandomAlphabet(){
 function setBackgroundColor(elementId){
     const element = document.getElementById(elementId);
     element.classList.add('bg-orange-400')
+}
+function removeBackgroundColor(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.remove('bg-orange-400')
 }
